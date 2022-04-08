94afab7fbd8aa42d2009e76675ac2219b60799d7
SleepyJake18 <94761866+SleepyJake18@users.noreply.github.com>
2022-04-08 14:21:16 -0400

    Basic structure and styling finished for header, footer, and home page

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+import '../index.css'
+
+export default function Footer () {
+    return (
+        <footer>
+            {/* <h2>
+                About Us
+            </h2> */}
+    <div className='column'>
+        <h3 className='whiteFont center footerNames'>
+            Jacob
+        </h3>
+        <div className='center'>
+
+        <ul className='footerUl'>
+            <li>
+                <a className='contactLink' href="mailto: jsmartin01@gmail.com" target="_blank">
+                <Icon icon="carbon:email" />
+                </a>
+            </li>
+            <li>
+                <a className='contactLink' href="https://github.com/SleepyJake18" target="_blank">
+                <Icon icon="akar-icons:github-fill" />
+                </a>
+            </li>
+            <li>
+                <a className='contactLink' href="https://www.linkedin.com/in/jacob-martin-779905222/" target="_blank">
+                <Icon icon="akar-icons:linkedin-box-fill" />
+                </a>
+            </li>
+            <li>
+                <a className='contactLink' href="https://stackoverflow.com/users/17466669/sleepyjake" target="_blank">
+                <Icon icon="mdi:stack-overflow" />
+                </a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    <div className='column'>
+        <h3 className='whiteFont center footerNames'>
+            Abdirahman
+        </h3>
+        
+        <ul className='footerUl'>
+            <li>
+                <a className='contactLink' href="mailto: jsmartin01@gmail.com" target="_blank">
+                <Icon icon="carbon:email" />
+                </a>
+            </li>
+            <li>
+                <a className='contactLink' href="https://github.com/SleepyJake18" target="_blank">
+                <Icon icon="akar-icons:github-fill" />
+                </a>
+            </li>
+            <li>
+                <a className='contactLink' href="https://www.linkedin.com/in/jacob-martin-779905222/" target="_blank">
+                <Icon icon="akar-icons:linkedin-box-fill" />
+                </a>
+            </li>
+            <li>
+                <a className='contactLink' href="https://stackoverflow.com/users/17466669/sleepyjake" target="_blank">
+                <Icon icon="mdi:stack-overflow" />
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div className='column'>
+        <h3 className='whiteFont center footerNames'>
+            Cody
+        </h3>
+        
+        <ul className='footerUl'>
+            <li>
+                <a className='contactLink' href="mailto: jsmartin01@gmail.com" target="_blank">
+                <Icon icon="carbon:email" />
+                </a>
+            </li>
+            <li>
+                <a className='contactLink' href="https://github.com/SleepyJake18" target="_blank">
+                <Icon icon="akar-icons:github-fill" />
+                </a>
+            </li>
+            <li>
+                <a className='contactLink' href="https://www.linkedin.com/in/jacob-martin-779905222/" target="_blank">
+                <Icon icon="akar-icons:linkedin-box-fill" />
+                </a>
+            </li>
+            <li>
+                <a className='contactLink' href="https://stackoverflow.com/users/17466669/sleepyjake" target="_blank">
+                <Icon icon="mdi:stack-overflow" />
+                </a>
+            </li>
+        </ul>
+    </div>
+    </footer>
+        );
+}
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../index.css'
 import { Link } from "react-router-dom";
+import { Icon } from '@iconify/react';
 
 function Header () {
     return (
@@ -8,40 +9,52 @@ function Header () {
       <nav className="navbar customNav fixed-top">
 
       <div className="container-fluid">
-        <a className="navbar-brand whiteFont" href="#">Social Trails</a>
+        <a className=" whiteFont customHeaderTitle align-self-center" href="#">Social Trails</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-          <span className="navbar-toggler-icon whiteFont"></span>
+          <span className="whiteFont customMenuIcon"><Icon icon="ant-design:menu-outlined" /></span>
         </button>
 
-        <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div className="offcanvas offcanvas-end customSideNav" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
 
           <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Social Trails</h5>
-            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <h5 className="offcanvas-title customHeaderTitle" id="offcanvasNavbarLabel">Social Trails</h5>
+            <button type="button" className="btn-close whiteFont" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
 
           <div className="offcanvas-body">
             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
 
               <li className="nav-item">
-                <Link className="whiteFont" aria-current="page" to={"/"}
+                <Link className="nav-link whiteFont customNavLink customNavLinkText" aria-current="page" to={"/"}
                 >Home</Link>
               </li>
               <li className="nav-item">
-                <Link className='nav-link whiteFont' aria-current="page" to={"/profile"}
+                <Link className='nav-link whiteFont customNavLink customNavLinkText' aria-current="page" to={"/profile"}
                 >Profile</Link>
               </li>
               <li className="nav-item">
-                <Link className='nav-link whiteFont' aria-current="page" to={"/traildata"}
+                <Link className='nav-link whiteFont customNavLink customNavLinkText' aria-current="page" to={"/traildata"}
                 >Trail Data</Link>
               </li>
               <li className="nav-item">
-                <Link className='nav-link whiteFont' aria-current="page" to={"/trailsposts"}
+                <Link className='nav-link whiteFont customNavLink customNavLinkText' aria-current="page" to={"/trailsposts"}
                 >Trail Posts</Link>
               </li>
               <li className="nav-item">
-                <Link className='nav-link whiteFont' aria-current="page" to={"/travel"}
+                <Link className='nav-link whiteFont customNavLink customNavLinkText' aria-current="page" to={"/travel"}
                 >Travel</Link>
+              </li>
+              <li className="nav-item">
+                <Link className='nav-link whiteFont customNavLink customNavLinkText' aria-current="page" to={"/travel"}
+                >Sign Up</Link>
+              </li>
+              <li className="nav-item">
+                <Link className='nav-link whiteFont customNavLink customNavLinkText' aria-current="page" to={"/travel"}
+                >Log In</Link>
+              </li>
+              <li className="nav-item">
+                <Link className='nav-link whiteFont customNavLink customNavLinkText' aria-current="page" to={"/travel"}
+                >Log Out</Link>
               </li>
 
             </ul>
