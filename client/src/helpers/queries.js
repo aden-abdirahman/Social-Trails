25dd686482e95ddf8b9c56e5c7381fb5ef7aff6d
aden-abdirahman <aden.abdirahman45@gmail.com>
2022-04-11 16:24:08 -0400

    .

---
+++ client/src/helpers/queries.js
@@ -0,0 +1,122 @@
+import { gql } from '@apollo/client';
+
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      trails {
+        _id
+        trailText
+        createdAt
+      }
+      trips {
+        _id
+        tripText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_TRAILS = gql`
+  query getTrails($lat: Float, $lon: Float) {
+    getTrails(lat: $lat, lon: $lon) {
+      id
+      trailText
+      trailAuthor
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_API_TRAILS = gql`
+  query getTrails {
+    getTrails {
+      id
+      name
+      length
+      description
+      directions
+      city
+      region
+      country
+      lat
+      lon
+      difficulty
+      features
+      rating
+      thumbnail
+    }
+  }
+`;
+
+export const QUERY_TRIPS = gql`
+  query getTrips {
+    trips {
+      _id
+      tripText
+      tripAuthor
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_SINGLE_TRAIL = gql`
+  query getSingleTrail($trailId: ID!) {
+    trail(trailId: $trailId) {
+      _id
+      trailText
+      trailAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
+export const QUERY_SINGLE_TRIP = gql`
+  query getSingleTrip($tripId: ID!) {
+    trip(tripId: $tripId) {
+      _id
+      tripText
+      tripAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      trails {
+        _id
+        trailText
+        trailAuthor
+        createdAt
+      }
+      trips {
+        _id
+        tripText
+        tripAuthor
+        createdAt
+      }
+    }
+  }
+`;
+
+
